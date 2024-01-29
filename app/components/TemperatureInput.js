478d0b226e681ef0846e99489bbf417d51c98540
Aaron <piafyoyo06@gmail.com>
2024-01-29 09:35:44 +0800

    feat: 匯出元件並美化行距

--- app/components/TemperatureInput.js
+++ app/components/TemperatureInput.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React from "react";
 
 // TemperatureInput元件用於單個溫度輸入
-function TemperatureInput({ scale, temperature, onTemperatureChange }) {
+export default function TemperatureInput({
+  scale,
+  temperature,
+  onTemperatureChange,
+}) {
   // 溫度刻度的標籤
   const scaleNames = {
-    c: '攝氏',
-    f: '華氏'
+    c: "攝氏",
+    f: "華氏",
   };
 
   return (
     <fieldset>
       <legend>輸入{scaleNames[scale]}溫度:</legend>
-      <input type="number" value={temperature}
-             onChange={(e) => onTemperatureChange(e, scale)} />
+      <input
+        type="number"
+        value={temperature}
+        onChange={(e) => onTemperatureChange(e, scale)}
+      />
     </fieldset>
   );
 }
